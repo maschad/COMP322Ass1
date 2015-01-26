9ff0a8cda8e536ab33299bc45b07552403b3a91a
cnehem <cnehem@lab2-3.CS.McGill.CA>
2015-01-26 11:02:56 -0500

    first comittt

---
+++ CS322_HW1/src/continuedStudent.cpp
@@ -0,0 +1,19 @@
+#include "continued.h"
+
+ContinuedFraction *getCFlargerThanOne(unsigned int b, unsigned int a) {
+  // your code here
+}
+
+ContinuedFraction *getCF(unsigned int b, unsigned int a) {
+  // your code here
+}
+
+
+ContinuedFraction *getCF(unsigned int head, ContinuedFraction *fixed, ContinuedFraction *period) {
+  // your code here
+}
+
+
+Fraction getApproximation(ContinuedFraction *fr, unsigned int n) {
+  // your code here
+}
