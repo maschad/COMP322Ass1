9d40caab9c2994417c4bf4fa59ae8ca5fcef8695
cnehem <cnehem@open-12.CS.McGill.CA>
2015-01-28 20:18:34 -0500

    Question 4 works

--- CS322_HW1/src/continuedStudent.cpp
+++ CS322_HW1/src/continuedStudent.cpp
@@ -11,17 +11,78 @@ unsigned int gcd(unsigned int a, unsigned int b)
 		return gcd(b%a,a);
 	}
 }
-ContinuedFraction *getCFlargerThanOne(unsigned int b, unsigned int a) {
-  // your code here
+
+unsigned int gcdFaster(unsigned int u, unsigned int v)
+{
+	 // simple cases (termination)
+	    if (u == v)
+	        return u;
+
+	    if (u == 0)
+	        return v;
+
+	    if (v == 0)
+	        return u;
+
+	    // look for factors of 2
+	    if (~u & 1) // u is even
+	    {
+	        if (v & 1) // v is odd
+	            return gcd(u >> 1, v);
+	        else // both u and v are even
+	            return gcd(u >> 1, v >> 1) << 1;
+	    }
+
+	    if (~v & 1) // u is odd, v is even
+	        return gcd(u, v >> 1);
+
+	    // reduce larger argument
+	    if (u > v)
+	        return gcd((u - v) >> 1, v);
+
+	    return gcd((v - u) >> 1, u);
+}
+ContinuedFraction *getCFlargerThanOne(unsigned int b, unsigned int a)
+{
+	ContinuedFraction *x = new ContinuedFraction;
+
+	if(a == 0)
+	{
+		(*x).head = b;
+		(*x).tail = nullptr;
+
+	}
+	else
+	{
+		ContinuedFraction *y = new ContinuedFraction;
+		(*y).head = b%a;
+		(*x).head = b/a * a;
+		(*x).tail = y;
+	}
+
+	return x;
 }
 
-ContinuedFraction *getCF(unsigned int b, unsigned int a) {
-  // your code here
+ContinuedFraction *getCF(unsigned int b, unsigned int a)
+{
+	ContinuedFraction *z = new ContinuedFraction;
+
+	if(b/a > 1)
+	{
+		z = getCFlargerThanOne(b,a);
+	}
+	else
+	{
+		z = getCFlargerThanOne(0,a);
+	}
+
+	return z;
 }
 
 
-ContinuedFraction *getCF(unsigned int head, ContinuedFraction *fixed, ContinuedFraction *period) {
-  // your code here
+ContinuedFraction *getCF(unsigned int head, ContinuedFraction *fixed, ContinuedFraction *period)
+{
+
 }
 
 
