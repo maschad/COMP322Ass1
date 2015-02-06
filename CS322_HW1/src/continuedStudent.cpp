64c5142b3621d875e1c092a29a59b12199e1fa89
cnehem <cnehem@csus-1.CS.McGill.CA>
2015-02-05 20:59:08 -0500

    works

--- CS322_HW1/src/continuedStudent.cpp
+++ CS322_HW1/src/continuedStudent.cpp
@@ -1,5 +1,6 @@
 #include "continued.h"
 
+/*recursive algorithm to find gcd*/
 unsigned int gcd(unsigned int a, unsigned int b)
 {
 	if(a == 0)
@@ -12,6 +13,8 @@ unsigned int gcd(unsigned int a, unsigned int b)
 	}
 }
 
+
+/*Uses integer division for gcd*/
 unsigned int gcdFaster(unsigned int u, unsigned int v)
 {
 	 // simple cases (termination)
@@ -54,10 +57,8 @@ ContinuedFraction *getCFlargerThanOne(unsigned int b, unsigned int a)
 	}
 	else
 	{
-		ContinuedFraction *y = new ContinuedFraction;
-		(*y).head = b%a;
-		(*x).head = b/a * a;
-		(*x).tail = y;
+		(*x).head = b/a;
+		(*x).tail = getCFlargerThanOne(a,b -a*x->head);
 	}
 
 	return x;
@@ -65,7 +66,7 @@ ContinuedFraction *getCFlargerThanOne(unsigned int b, unsigned int a)
 
 ContinuedFraction *getCF(unsigned int b, unsigned int a)
 {
-	ContinuedFraction *z = new ContinuedFraction;
+	ContinuedFraction *z;
 
 	if(b/a > 1)
 	{
@@ -73,28 +74,51 @@ ContinuedFraction *getCF(unsigned int b, unsigned int a)
 	}
 	else
 	{
-		z = getCFlargerThanOne(0,a);
+		z = new ContinuedFraction;
+		(*z).head = 0;
+		(*z).tail = getCFlargerThanOne(a,b);
 	}
 
 	return z;
 }
 
+void cat(ContinuedFraction *frac, unsigned int a)
+{
+	while(frac->tail != nullptr)
+	{
+		frac = frac->tail;
+	}
+	ContinuedFraction *x = new ContinuedFraction;
+	(*x).head = a;
+	(*x).tail = nullptr;
+	frac->tail = x;
+}
+
+void cat(ContinuedFraction *frac , ContinuedFraction *end) {
+	if(end == nullptr)
+	{
+		return;
+	}
+	else
+	{
+		cat(frac,end->head);
+		cat(frac,end->tail);
+	}
+}
 
 ContinuedFraction *getCF(unsigned int head, ContinuedFraction *fixed, ContinuedFraction *period)
 {
 	ContinuedFraction *x = new ContinuedFraction;
 	(*x).head = head;
+	(*x).tail = nullptr;
 
-	if(period->tail == nullptr)
-	{
-		return x;
-	}
-	else
-	{
-		ContinuedFraction *fTail = fixed->tail;
-		ContinuedFraction *pTail = period->tail;
-		return getCF(fixed->head,fTail,pTail);
-	}
+	cat(x,fixed);
+	ContinuedFraction *y, *z;
+	for(y = x; y->tail != nullptr; y = y->tail);
+	cat(x,period);
+	for(z = y; z->tail != nullptr; z = z->tail);
+	z->tail = y->tail;
+	return x;
 
 }
 
@@ -103,16 +127,23 @@ Fraction getApproximation(ContinuedFraction *fr, unsigned int n)
 {
 	Fraction y;
 	y.numerator = fr->head;
-	y.denominator = fr->tail->head;
+	y.denominator = 1;
 
-	if(n == 0)
+	if(n == 1 || fr->head == 0)
 	{
 		return y;
 
 	}
 	else
 	{
-		return getApproximation(fr->tail,n-1);
+		Fraction x = getApproximation(fr->tail,n-1);
+		Fraction temp;
+		temp.numerator = x.numerator;
+		temp.denominator = x.denominator;
+		x.denominator = temp.numerator;
+		x.numerator = temp.denominator;
+		x.numerator = x.numerator + x.denominator*fr->head;
+		return x;
 	}
 
 }
